32e74cea22f0e5fdb8e702a645a92d19eb818158
madshines <63632979+madshines@users.noreply.github.com>
2020-05-28 20:55:12 +0800

    elasticsearch测试

---
+++ SpringBootES/src/main/java/com/madshines/springbootes/SpringbootesApplication.java
@@ -0,0 +1,13 @@
+package com.madshines.springbootes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootesApplication.class, args);
+    }
+
+}
---
+++ SpringBootES/src/test/java/com/madshines/springbootes/SpringbootesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madshines.springbootes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
